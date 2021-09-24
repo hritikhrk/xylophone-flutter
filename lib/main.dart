c453966b20bc023db5b19635ea5e74af5d58aad5
Hritik Kumar <hritik.kmr.111@gmail.com>
2021-09-24 22:05:36 +0530

    add buttons and sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                onPressed: () {
+                  playSound(1);
+                },
+                child: SizedBox(
+                  width: 100,
+                  height: 40.0,
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                ),
+                onPressed: () {
+                  playSound(2);
+                },
+                child: SizedBox(
+                  width: 100,
+                  height: 40.0,
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                ),
+                onPressed: () {
+                  playSound(3);
+                },
+                child: SizedBox(
+                  width: 100,
+                  height: 40.0,
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+                onPressed: () {
+                  playSound(4);
+                },
+                child: SizedBox(
+                  width: 100,
+                  height: 40.0,
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                ),
+                onPressed: () {
+                  playSound(5);
+                },
+                child: SizedBox(
+                  width: 100,
+                  height: 40.0,
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () {
+                  playSound(6);
+                },
+                child: SizedBox(
+                  width: 100,
+                  height: 40.0,
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.indigo,
+                ),
+                onPressed: () {
+                  playSound(7);
+                },
+                child: SizedBox(
+                  width: 100,
+                  height: 40.0,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
